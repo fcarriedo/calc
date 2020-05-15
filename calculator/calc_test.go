08de0e84284ee040df4b63087db0d1fd7f88c39f
Francisco Carriedo <fcarriedo@gmail.com>
2020-05-15 16:25:38 -0500

    Add test coverage

--- calculator/calc_test.go
+++ calculator/calc_test.go
@@ -8,4 +8,12 @@ func TestCalculate(t *testing.T) {
 	if Mul(2, 4) != 8 {
 		t.Error("Expected 2 * 4 to equal 8")
 	}
+
+	if Mul(2, 4) != 8 {
+		t.Error("Expected 2 * 4 to equal 8")
+	}
+
+	if Mul(2, 4) != 8 {
+		t.Error("Expected 2 * 4 to equal 8")
+	}
 }
