e5d5932744a9d38e6474632b8ad78656a3ef3682
Francisco Carriedo <fcarriedo@gmail.com>
2020-05-15 17:24:33 -0500

    Add test coverage for Div op

--- calculator/calc_test.go
+++ calculator/calc_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestCalculate(t *testing.T) {
+func TestMul(t *testing.T) {
 	if Mul(2, 4) != 8 {
 		t.Error("Expected 2 * 4 to equal 8")
 	}
@@ -15,5 +15,19 @@ func TestCalculate(t *testing.T) {
 
 	if Mul(2, 4) != 8 {
 		t.Error("Expected 2 * 4 to equal 8")
+	}
+}
+
+func TestDiv(t *testing.T) {
+	if Div(4, 4) != 1 {
+		t.Error("Expected 4 / 4 to equal 1")
+	}
+
+	if Div(4, 2) != 2 {
+		t.Error("Expected 4 / 2 to equal 2")
+	}
+
+	if Div(3, 2) != 1 {
+		t.Error("Expected 3 / 2 to equal 1")
 	}
 }
