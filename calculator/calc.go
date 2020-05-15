132160c524f90477be7d4aa8176e793432c76484
Francisco Carriedo <fcarriedo@gmail.com>
2020-05-15 18:21:57 -0500

    Update error message

--- calculator/calc.go
+++ calculator/calc.go
@@ -12,7 +12,7 @@ func Mul(x int, y int) int {
 
 func Div(x int, y int) (int, error) {
 	if y == 0 {
-		return 0, errors.New("Division by 0 is undefined")
+		return 0, errors.New("Division by 0 is 'undefined'")
 	}
 
 	return x / y, nil
