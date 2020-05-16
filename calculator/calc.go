47a625b19515e32f60ece585e212764851ff7594
Francisco Carriedo <fcarriedo@gmail.com>
2020-05-15 22:58:50 -0500

    Add documentation to package methods

--- calculator/calc.go
+++ calculator/calc.go
@@ -5,14 +5,18 @@ import (
 	"github.com/thatisuday/nummanip/calc"
 )
 
+// Sums all the numbers given as arguments
 func Sum(nums ...int) int {
 	return calc.Add(nums...)
 }
 
+// Multiplies the 2 numbers given as arguments
 func Mul(x int, y int) int {
 	return x * y
 }
 
+// Divides x / y.
+// Will return error if y = 0 (as div by Zero is undefined)
 func Div(x int, y int) (int, error) {
 	if y == 0 {
 		return 0, errors.New("Division by 0 is undefined")
